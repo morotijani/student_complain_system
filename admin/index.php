0d612f68043a7b5f96f1f03b2d6ddc9c35b1ea90
morotijani <tjhackx111@gmail.com>
2024-06-24 17:01:55 +0000

    initial commit

--- admin/index.php
+++ admin/index.php
@@ -240,7 +240,7 @@
     <main>
         <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
             <div class="col-md-6 p-lg-5 mx-auto my-5">
-                <h1 class="display-3 fw-bold">there is <?= count_complaints(); ?>244,234 compliants</h1>
+                <h1 class="display-3 fw-bold">there is <?= count_complaints(); ?> compliants</h1>
                 <h3 class="fw-normal text-muted mb-3">from the database so far</h3>
                 <div class="d-flex gap-3 justify-content-center lead fw-normal">
                     <a class="icon-link" href="complaints">
